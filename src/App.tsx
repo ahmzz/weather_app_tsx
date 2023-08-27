0e42b0a06c5e50f436949d6abda0a19b68935f36
ahmzz <ahmedminhas247@outlook.com>
2023-08-27 23:52:09 +0500

    Geolocation API applied usng fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useState } from 'react'
 
-const App = ():JSX.Element => {
+const App = (): JSX.Element => {
+  const [place, setPlace] = useState<string>('')
+  const [options, setOptions] = useState<[]>([])
 
-  const [place,setPlace]=useState<string>('')
+  const getSearchOptions = (value: string) => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}
+    `)
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
 
-  const onInputChange=(e:ChangeEvent<HTMLInputElement>)=>{
-    setPlace(e.target.value)
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPlace(e.target.value.trim())
+
+    if (e.target.value === '') return
+
+    getSearchOptions(e.target.value)
   }
 
   return (
@@ -14,16 +25,33 @@ const App = ():JSX.Element => {
         className="bg-[#708D81]  bg-opacity-40 backdrop-blur-lg drop-shadow-md rounded w-full md:max-w-[500px]
         p-4 flex flex-col text-center items-center justify-center md:px-18 lg:px24 h-full lg:h-[500px] text-white"
       >
-        <h1 className=" text-4xl font-thin">Weather <span className=" font-black  text-[#ffb703] ">Forecast </span></h1>
-        <p className=" text-sm mt-3 italic">Want to know what is the weather like at your next travel destination?</p>
+        <h1 className=" text-4xl font-thin">
+          Weather{' '}
+          <span className=" font-black  text-[#ffb703] ">Forecast </span>
+        </h1>
+        <p className=" text-sm mt-3 italic">
+          Want to know what is the weather like at your next travel destination?
+        </p>
 
-<div className="flex mt-10 md:mt-4">
-        <input type="text" value={place} onChange={onInputChange} className=" text-[#001427] px-2 py-1 rounded-l-md border bg-[#fff2b2] border-white"/>
-        <button className=" text-sm rounded-r-md border bg-[#001427] border-white hover:border-[#ffb703] hover:text-[#ffb703] text-zinc-100 px-2 py cursor-pointer">
-          search
-        </button>
+        <div className="relative flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={place}
+            onChange={onInputChange}
+            className=" text-[#001427] px-2 py-1 rounded-l-md border bg-[#fff2b2] border-white"
+          />
+
+          <ul className=' absolute top-9 bg-[#fff2b2] ml-1 rounded-b-md text-black '>
+          {options.map((option: { name: string },index:number) => (
+            <li key={option.name+'-'+index}>
+              <button className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'>{option.name}</button>
+            </li>
+          ))}
+          </ul>
+          <button className=" text-sm rounded-r-md border bg-[#001427] border-white hover:border-[#ffb703] hover:text-[#ffb703] text-zinc-100 px-2 py cursor-pointer">
+            search
+          </button>
         </div>
-
       </section>
     </main>
   )
@@ -32,4 +60,3 @@ const App = ():JSX.Element => {
 export default App
 
 //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
